9f876670bcb29dfa0b05fec9919078a2152c8b4f
aminnoorzai222-hub <aminnoorzai222@gmail.com>
2026-04-14 06:26:30 +0430

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const HF_TOKEN = "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"; // 
+
 app.get("/", (req, res) => {
   res.send("Aidly AI Server is running ✅");
 });
@@ -13,23 +15,31 @@ app.get("/", (req, res) => {
 app.post("/chat", async (req, res) => {
   const message = req.body.message;
 
-  if (!message) {
-    return res.json({ reply: "No message" });
-  }
-
   try {
     const response = await fetch(
-      "https://api.affiliateplus.xyz/api/chatbot?message=" + encodeURIComponent(message)
+      "https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + HF_TOKEN,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          inputs: message
+        })
+      }
     );
 
     const data = await response.json();
 
-    res.json({ reply: data.message });
+    res.json({
+      reply: data.generated_text || "AI error ❌"
+    });
 
   } catch (err) {
-    res.json({ reply: "AI not available ❌" });
+    res.json({ reply: "Server error ❌" });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, () => console.log("Server running")
